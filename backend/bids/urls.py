cedd48861e20f636ac2f5e712f66444db7bc8f33
Bhakta84 <bhaktaghalley5@gmail.com>
2026-09-10 12:04:57 +0600

    Fix bid document upload route

--- backend/bids/urls.py
+++ backend/bids/urls.py
@@ -1,6 +1,6 @@
 from rest_framework.routers import DefaultRouter
 from .views import BidViewSet,BidDocumentViewSet
 router=DefaultRouter()
-router.register("",BidViewSet,basename="bid")
 router.register("documents",BidDocumentViewSet,basename="bid-document")
+router.register("",BidViewSet,basename="bid")
 urlpatterns=router.urls
